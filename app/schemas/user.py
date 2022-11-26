f48801f337d4e2e81a9b789c7f6ae519d3bddc33
mannydwal209 <manvir.dhaliwal@sjsu.edu>
2022-11-25 16:19:25 -0800

    BAC-9: add user registration route, with email validation

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,4 +1,11 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
+
+
+class NewUserIn(BaseModel):
+    firstname: str = Field(..., description="The first name of the user")
+    lastname: str = Field(..., description="The last name of the user")
+    username: EmailStr = Field(..., description="The username of user")
+    password: str = Field(..., description="The password of the user", min_length=8, max_length=32)
 
 
 class UserContext(BaseModel):
@@ -7,7 +14,7 @@ class UserContext(BaseModel):
                            description="The firstname of the user", example="John")
     lastname: str = Field(...,
                           description="The lastname of the user", example="Doe")
-    email: str = Field(..., description="The email of the user",
+    email: EmailStr = Field(..., description="The email of the user",
                        example="john.doe@gmail.com")
     is_superuser: bool = Field(...,
                                description="Whether the user is a superuser", example=False)
