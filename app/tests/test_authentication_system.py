fd7f491b3c36ecac297d93b108bbc467839733f0
NoahCardoza <noahcardoza@gmail.com>
2022-11-28 08:23:16 -0800

    BAC-43: add tests for registration and fix registration route

--- app/tests/test_authentication_system.py
+++ app/tests/test_authentication_system.py
@@ -4,6 +4,7 @@ import json
 from httpx import AsyncClient
 
 AUTH_TOKEN_ENDPOINT = "/auth/token/"
+AUTH_REGISTER_ENDPOINT = "/auth/register"
 
 
 def decode_auth_token(auth_token):
@@ -78,3 +79,75 @@ async def test_valid_customer_login(client):
     assert user['email'] == email;
     assert not user['is_superuser']
     assert not user['is_employee']
+
+
+async def test_register_new_user_short_password(client):
+    email = "john.malkovich@sjsu.edu"
+    password = "TAsqf"
+
+    response = await client.post(AUTH_REGISTER_ENDPOINT, json={
+        "firstname": "John",
+        "lastname": "Malkovich",
+        "username": email,
+        "password": password,
+    })
+    
+    assert response.status_code == 422
+
+    data = response.json()[0]
+    assert data['loc'] == ['body', 'password']
+    
+
+async def test_register_new_user_long_password(client):
+    email = "jim.carrey@sjsu.edu"
+    password = "4f79ce357d3173545055e5a33a710cec4f79ce357d3173545055e5a33a710cec"
+
+    response = await client.post(AUTH_REGISTER_ENDPOINT, json={
+        "firstname": "Jim",
+        "lastname": "Carrey",
+        "username": email,
+        "password": password,
+    })
+    
+    assert response.status_code == 422
+    
+    data = response.json()[0]
+    assert data['loc'] == ['body', 'password']
+
+
+async def test_register_new_user_and_login(client):
+    email = "emma.stone@sjsu.edu"
+    password = "4f79ce357d3173545055e5a33a710cec"
+
+    response = await client.post(AUTH_REGISTER_ENDPOINT, json={
+        "firstname": "Emma",
+        "lastname": "Stone",
+        "username": email,
+        "password": password,
+    })
+    
+    assert response.status_code == 200
+    
+    data = response.json()
+    assert 'access_token' in data
+
+    response = await client.post(AUTH_TOKEN_ENDPOINT, data={
+        "username": email,
+        "password": password
+    })
+    
+    data = response.json()
+
+    assert response.status_code == 200
+    assert 'access_token' in data
+
+
+async def test_register_new_user_with_duplicate_email(client):
+    response = await client.post(AUTH_REGISTER_ENDPOINT, json={
+        "firstname": "Morganna",
+        "lastname": "Freewoman",
+        "username": "morgan.freemen@sjsu.edu",
+        "password": "ce7860b44604696b24e2",
+    })
+
+    assert response.status_code == 409
